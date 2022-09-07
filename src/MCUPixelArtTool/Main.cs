7f00efc91a3901232cb45ddc97cdb925bb9c707d
Pop-Apple <101918076+Pop-Apple@users.noreply.github.com>
2022-09-07 19:00:54 +0900

    Update Main.cs

--- src/MCUPixelArtTool/Main.cs
+++ src/MCUPixelArtTool/Main.cs
@@ -1,9 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Windows.Forms;
 using System.IO;
+using System.Windows.Forms;
 
 namespace PixelArtTool
 {
